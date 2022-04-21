d99f58b8668e6f6a61f3b264686ddaba90729a0c
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-04-20 23:30:19 -0400

    fix: enable semantic release (#13)
    Close #4

---
+++ release.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  repositoryUrl: "https://github.com/arkinmodi/resume",
+  branches: ["main"],
+  tagFormat: "v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs", "package.json", "package-lock.json"],
+      },
+    ],
+  ],
+  debug: true,
+};
