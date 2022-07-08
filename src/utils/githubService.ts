9a98016f03cfedc989ad92b6121676744fa63c0c
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-07-07 21:41:10 -0400

    feat: get repo data from github api

---
+++ src/utils/githubService.ts
@@ -0,0 +1,51 @@
+import { Endpoints } from "@octokit/types";
+
+export type GetRepositoryMetadataInput = {
+  owner: string;
+  repo: string;
+  displayName: string;
+};
+
+export type RepositoryMetadata = {
+  url: string;
+  homepage: string;
+  displayName: string;
+  description: string;
+};
+
+export const getRepositoriesMetadata: (
+  repos: GetRepositoryMetadataInput[]
+) => Promise<RepositoryMetadata[]> = async (repositories) => {
+  const allRepoMetadata: RepositoryMetadata[] = [];
+
+  for (const repo of repositories) {
+    const repoMetadata = await getRepo(repo.owner, repo.repo);
+    allRepoMetadata.push({
+      url: repoMetadata.html_url,
+      homepage: repoMetadata.homepage,
+      displayName: repo.displayName,
+      description: repoMetadata.description,
+    });
+  }
+
+  return allRepoMetadata;
+};
+
+type GetRepoResponseData =
+  Endpoints["GET /repos/{owner}/{repo}"]["response"]["data"];
+
+const getRepo: (
+  owner: string,
+  repo: string
+) => Promise<GetRepoResponseData> = async (owner, repo) => {
+  const response = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+  const data: GetRepoResponseData = await response.json();
+
+  if (response.ok) {
+    return data;
+  } else {
+    return Promise.reject(
+      new Error(`Failed to find ${owner}/${repo}. Response Body: ${data}`)
+    );
+  }
+};
